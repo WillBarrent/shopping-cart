9ea3443b01e7462be8f436dab44713c9813b61ad
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-02 21:15:51 +0500

    Add Header with navigaion

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import styles from "./Header.module.css";
+import { Store, ShoppingCart, ShoppingBasket } from "lucide-react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.navLogo}>
+          <ShoppingBasket size={48} color="white" />
+        </div>
+        <ul className={styles.navigation}>
+          <li className={styles.navigationItem}>
+            <Link className={styles.navigationLink} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={styles.navigationItem}>
+            <Link className={styles.navigationLink} to="/shop">
+              Shop
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.shoppingCart}>
+          <Link className={styles.navigationLink} to="/cart">
+            <ShoppingCart size={48}/>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
