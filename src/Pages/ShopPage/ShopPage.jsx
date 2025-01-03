6c49e1c70a92b3a0a9021d35397f75a8081cda1c
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-03 19:39:41 +0500

    Implement increment and decrement component and functionality

--- src/Pages/ShopPage/ShopPage.jsx
+++ src/Pages/ShopPage/ShopPage.jsx
@@ -3,6 +3,7 @@ import styles from "./ShopPage.module.css";
 
 function ShopPage() {
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     let ignore = false;
 
@@ -15,8 +16,18 @@ function ShopPage() {
               mode: "cors",
             }
           );
-          const data = await response.json();
-          console.log(data);
+
+          let data = await response.json();
+          data = data.map((datum) => {
+            return {
+              id: datum.id,
+              image: datum.images[0].slice(datum.images[0].indexOf("h")),
+              title: datum.title,
+              price: datum.price,
+              count: 1,
+            };
+          });
+
           setProducts(data);
         })();
       }
@@ -29,25 +40,65 @@ function ShopPage() {
     };
   }, []);
 
+  function countIncrement(product, index) {
+    const updatedProduct = product;
+    updatedProduct.count += 1;
+    const updatedProductsList = products.map((product) => product);
+    updatedProductsList[index] = updatedProduct;
+    setProducts(updatedProductsList);
+  }
+
+  function countDecrement(product, index) {
+    const updatedProduct = product;
+    if (updatedProduct.count > 1) {
+      updatedProduct.count -= 1;
+    }
+    const updatedProductsList = products.map((product) => product);
+    updatedProductsList[index] = updatedProduct;
+    setProducts(updatedProductsList);
+  }
+
   return (
     <main className={styles.shop}>
       {products.length === 0 ? (
         <div className={styles.loading}>Loading...</div>
       ) : (
         <div className={styles.products}>
-          {products.map((product) => {
+          {products.map((product, index) => {
             return (
               <div className={styles.product} key={product.id}>
                 <div className={styles.productImage}>
                   <img
                     className={styles.productImg}
-                    src={product.images[0]}
+                    src={product.image}
                     alt="Product image"
                   />
                 </div>
                 <div className={styles.productTitle}>{product.title}</div>
                 <div className={styles.productPrice}>{product.price}$</div>
-                <button className={styles.addToCart}>Add To Cart</button>
+                <div className={styles.addToCart}>
+                  <div className={styles.productCount}>
+                    <button
+                      onClick={() => countDecrement(product, index)}
+                      className={styles.countDecrement}
+                    >
+                      -
+                    </button>
+                    <input
+                      className={styles.countNumber}
+                      type="text"
+                      disabled
+                      value={product.count}
+                    />
+                    <button
+                      onClick={() => countIncrement(product, index)}
+                      className={styles.countIncrement}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <button className={styles.addToCartBtn}>Add To Cart</button>
+                </div>
               </div>
             );
           })}
