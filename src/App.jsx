6a8f43ce65eb1c7773d7d8c7dcd24094cf71e36c
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-04 21:38:17 +0500

    Create shopping cart array that contains products that you add in cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,29 @@ import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
 import ShopPage from "./Pages/ShopPage/ShopPage";
 import CartPage from "./Pages/CartPage/CartPage";
+import { useState } from "react";
 
 function App() {
   const location = useLocation();
   const locationPathName = location.pathname;
 
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  function addToCart(product) {
+    const updatedCart = [...shoppingCart, product];
+    setShoppingCart(updatedCart);
+  }
+
   return (
     <>
       <Header />
-      {locationPathName === "/shop" ? <ShopPage /> : (locationPathName === "/cart") ? <CartPage /> : <HomePage />}
+      {locationPathName === "/shop" ? (
+        <ShopPage shoppingCart={shoppingCart} addToCart={addToCart} />
+      ) : locationPathName === "/cart" ? (
+        <CartPage shoppingCart={shoppingCart} />
+      ) : (
+        <HomePage />
+      )}
     </>
   );
 }
