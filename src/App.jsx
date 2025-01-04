0e9a2ee99ea03e23e2482a7a994039c814ba5264
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-04 20:49:54 +0500

    Add Cart Page to the location path check condition in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage/HomePage";
 import ShopPage from "./Pages/ShopPage/ShopPage";
+import CartPage from "./Pages/CartPage/CartPage";
 
 function App() {
   const location = useLocation();
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Header />
-      {locationPathName === "/shop" ? <ShopPage /> : <HomePage />}
+      {locationPathName === "/shop" ? <ShopPage /> : (locationPathName === "/cart") ? <CartPage /> : <HomePage />}
     </>
   );
 }
