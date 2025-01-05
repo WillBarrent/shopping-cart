1a49b948c4a6d9b118406337d0cf63de0f079c66
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-05 18:51:11 +0500

    Provide all functionality to the Cart page and Cart Component

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,35 +1,60 @@
 import PropTypes from "prop-types";
 import styles from "./Cart.module.css";
+import { CircleX } from "lucide-react";
 
 function Cart({
-    imageUrl,
-    title,
-    count,
-    price
+  id,
+  imageUrl,
+  title,
+  count,
+  price,
+  removeFromCart,
+  cartCountIncrement,
+  cartCountDecrement,
 }) {
   return (
     <div className={styles.cart}>
       <div className={styles.cartImage}>
-        <img
-          className={styles.cartImg}
-          src={imageUrl}
-          alt=""
-        />
+        <img className={styles.cartImg} src={imageUrl} alt="" />
         <div className={styles.cartTitle}>{title}</div>
       </div>
       <div className={styles.cartCount}>
-        <button className={styles.countDecrement}>-</button>
-        <input className={styles.countNumber} type="text" disabled value={count} />
-        <button className={styles.countIncrement}>+</button>
+        <button
+          onClick={() => cartCountDecrement(id)}
+          className={styles.countDecrement}
+        >
+          -
+        </button>
+        <input
+          className={styles.countNumber}
+          type="text"
+          disabled
+          value={count}
+        />
+        <button
+          onClick={() => cartCountIncrement(id)}
+          className={styles.countIncrement}
+        >
+          +
+        </button>
       </div>
       <div className={styles.cartCost}>{price * count}$</div>
+      <div className={styles.cartRemove} onClick={() => removeFromCart(id)}>
+        <CircleX className={styles.removeBtn} size={30} color="red" />
+      </div>
     </div>
   );
 }
 
 Cart.propTypes = {
-    imageUrl: PropTypes.string,
-    title: PropTypes.string,
-    count: PropTypes.number,
-    price: PropTypes.number,
-}
+  id: PropTypes.number,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+  count: PropTypes.number,
+  price: PropTypes.number,
+  removeFromCart: PropTypes.func,
+  cartCountIncrement: PropTypes.func,
+  cartCountDecrement: PropTypes.func,
+};
+
+export default Cart;
--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,11 +1,26 @@
 import { ShoppingBasket } from "lucide-react";
 import PropTypes from "prop-types";
 import styles from "./CartPage.module.css";
+import Cart from "../../Components/Cart/Cart";
 
 function CartPage({
-  shoppingCart
+  shoppingCart,
+  removeFromCart,
+  cartCountIncrement,
+  cartCountDecrement,
 }) {
-  console.log(shoppingCart);
+  function totalPriceCount() {
+    let totalPrice = 0;
+
+    for (let i = 0; i < shoppingCart.length; i++) {
+      totalPrice += shoppingCart[i].price * shoppingCart[i].count;
+    }
+
+    return totalPrice;
+  }
+
+  const totalPrice = totalPriceCount();
+
   return (
     <main className={styles.shoppingCart}>
       <div className={styles.cartInfo}>
@@ -13,8 +28,24 @@ function CartPage({
         <h1 className={styles.cartTitle}>Shopping Cart</h1>
       </div>
       <div className={styles.carts}>
-        
-        
+        {shoppingCart.map((cart) => {
+          return (
+            <Cart
+              key={cart.id}
+              id={cart.id}
+              count={cart.count}
+              imageUrl={cart.imageUrl}
+              price={cart.price}
+              title={cart.title}
+              removeFromCart={removeFromCart}
+              cartCountIncrement={cartCountIncrement}
+              cartCountDecrement={cartCountDecrement}
+            />
+          );
+        })}
+      </div>
+      <div className={styles.cartTotal}>
+        <div className={styles.totalPrice}>Total: {totalPrice}$</div>
       </div>
     </main>
   );
@@ -22,6 +53,9 @@ function CartPage({
 
 CartPage.propTypes = {
   shoppingCart: PropTypes.array,
-}
+  removeFromCart: PropTypes.func,
+  cartCountIncrement: PropTypes.func,
+  cartCountDecrement: PropTypes.func,
+};
 
 export default CartPage;
