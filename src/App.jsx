278a064535c2e94dcb95c1e98a526cece618f554
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-05 18:49:51 +0500

    Create Shopping Cart Add, Remove and increment/decrement functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,60 @@ function App() {
 
   const [shoppingCart, setShoppingCart] = useState([]);
 
-  function addToCart(product) {
+  function addToCart(product, count) {
+    const productInCart = shoppingCart.find((cart) => cart.id === product.id);
+    if (productInCart) {
+      const productInCartCount = productInCart.count;
+      const productInCartIndex = shoppingCart.findIndex(
+        (cart) => cart.id === product.id
+      );
+      const updatedCart = [...shoppingCart];
+      updatedCart[productInCartIndex] = {
+        ...updatedCart[productInCartIndex],
+        count: productInCartCount + count,
+      };
+
+      setShoppingCart(updatedCart);
+
+      return;
+    }
+
     const updatedCart = [...shoppingCart, product];
+    setShoppingCart(updatedCart);
+  }
+
+  function removeFromCart(productId) {
+    const productInCartIndex = shoppingCart.findIndex(
+      (cart) => cart.id === productId
+    );
+    const updatedCart = [...shoppingCart];
+    updatedCart.splice(productInCartIndex, 1);
+    setShoppingCart(updatedCart);
+  }
+
+  function cartCountIncrement(productId) {
+    const productInCartIndex = shoppingCart.findIndex(
+      (cart) => cart.id === productId
+    );
+    const updatedCart = [...shoppingCart];
+
+    updatedCart[productInCartIndex].count += 1;
+
+    setShoppingCart(updatedCart);
+  }
+
+  function cartCountDecrement(productId) {
+    const productInCartIndex = shoppingCart.findIndex(
+      (cart) => cart.id === productId
+    );
+    const updatedCart = [...shoppingCart];
+
+    if (updatedCart[productInCartIndex].count === 1) {
+      return;
+    }
+
+    updatedCart[productInCartIndex].count -= 1;
+
     setShoppingCart(updatedCart);
   }
 
@@ -23,7 +75,12 @@ function App() {
       {locationPathName === "/shop" ? (
         <ShopPage shoppingCart={shoppingCart} addToCart={addToCart} />
       ) : locationPathName === "/cart" ? (
-        <CartPage shoppingCart={shoppingCart} />
+        <CartPage
+          shoppingCart={shoppingCart}
+          removeFromCart={removeFromCart}
+          cartCountIncrement={cartCountIncrement}
+          cartCountDecrement={cartCountDecrement}
+        />
       ) : (
         <HomePage />
       )}
