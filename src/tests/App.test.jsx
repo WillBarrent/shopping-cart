a7c628e58e4b0f5b1bdabf3721b82ea1fbd3cae3
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-01-06 21:01:02 +0500

    Test that pages rendering correct and handle bad routes

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,14 +1,39 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("App component", () => {
   it("renders header", () => {
-    render(
-        <App />,
-        {wrapper: BrowserRouter}
-    );
+    render(<App />, { wrapper: BrowserRouter });
+
     expect(screen.getByRole("banner")).toBeInTheDocument();
+  });
+
+  it("rendering pages correctly", async () => {
+    render(<App />, { wrapper: BrowserRouter });
+
+    const user = userEvent.setup();
+
+    expect(screen.getByText("Shopping Cart")).toBeInTheDocument();
+    await user.click(screen.getByText("Shop"));
+    expect(screen.getByText("Product Page")).toBeInTheDocument();
+    await user.click(screen.getByTestId("cart"));
+    expect(screen.getByText("Shopping Cart")).toBeInTheDocument();
+  });
+
+  it("rendering a bad route with error", () => {
+    const badRoute = "/bad-route";
+
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByText(/Maybe this page doesn't exist./i)
+    ).toBeInTheDocument();
   });
 });
